34ff1083b2cbd5d369ce47a15a095752ab4d1370
Mitch <michicesa03@gmail.com>
2023-11-30 16:12:16 +0100

    - Implementate funzioni 1, 2, 3

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,12 +5,28 @@ LIST_HEAD(pcbFree_h);
 static int next_pid = 1;
 
 void initPcbs() {
+    for(int i = 0; i < MAXPROC; i++){
+        list_add(&pcbTable[i].p_list, &pcbFree_h);
+    }
 }
 
 void freePcb(pcb_t *p) {
+    list_add_tail(&(p->p_list), &pcbFree_h);
 }
 
 pcb_t *allocPcb() {
+    if(list_empty(&pcbFree_h))
+        return NULL;
+    else{
+        pcb_PTR tempPcb = container_of(pcbFree_h.next, pcb_t, p_list);
+        list_del(pcbFree_h.next);
+        INIT_LIST_HEAD(&tempPcb->p_list);
+        INIT_LIST_HEAD(&tempPcb->p_child);
+        INIT_LIST_HEAD(&tempPcb->p_sib);
+        INIT_LIST_HEAD(&tempPcb->msg_inbox);
+        tempPcb->p_parent = NULL;
+        return tempPcb;
+    }
 }
 
 void mkEmptyProcQ(struct list_head *head) {
