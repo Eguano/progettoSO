80f7fe14b7abf7214279051e49059bcfe1b4d048
Michele Cesari <michele.cesari6@studio.unibo.it>
2024-09-15 18:50:21 +0200

    Refactor e commenti vmSupport

--- phase3/vmSupport.h
+++ phase3/vmSupport.h
@@ -6,16 +6,15 @@
 #define VMSUPPORT_H
 
 #include <umps/libumps.h>
+#include <umps/arch.h>
 #include "../headers/const.h"
 #include "../headers/types.h"
-#include <umps/arch.h>
 
+void uTLB_RefillHandler();
 void Pager();
-
 static unsigned int selectFrame();
 void invalidateFrame(unsigned int frame, support_t *support_PTR);
 void updateTLB(pteEntry_t *entry);
-
 int readWriteBackingStore(dtpreg_t *flashDevReg, memaddr dataMemAddr, unsigned int devBlockNo, unsigned int opType);
 
 #endif
